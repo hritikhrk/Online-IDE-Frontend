8573149a87d9a35c4ccf10c5775efc73a36d02bd
Hritik Kumar <hritik.kmr.111@gmail.com>
2023-02-10 13:02:25 +0530

    modify config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
-export const SECRET_KEY = {
-  URL: "https://online-ide-api.onrender.com",
-};
+// export const SECRET_KEY = {
+//   URL: "https://online-ide-api.onrender.com",
+// };
 
-//https://online-ide-api.onrender.com/
+export const SECRET_KEY = {
+  URL: "",
+};
